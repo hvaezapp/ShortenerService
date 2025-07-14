d9bbd357e25c1fd074fd033be3a129c1fcf25762
hassa <hassa@LAPTOP-U03CN6LR>
2025-07-14 15:11:22 +0330

    add scalar and entity model

--- src/ShortenerService/Program.cs
+++ src/ShortenerService/Program.cs
@@ -1,5 +1,21 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+
+
+app.UseHttpsRedirection();
 app.Run();
---
+++ src/ShortenerService/UrlDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace ShortenerService
+{
+    public class UrlDetails
+    {
+        public string? LongUrl { get; set; }
+        public string? ShortCode { get; set; }
+    }
+}
